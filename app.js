a04cd6afcf1419cdf00f0e1744c17bcd8daf91d2
Sophie Roudier <sophie.roudier@hotmail.fr>
2022-06-07 17:12:39 +0200

    Installation Mongoose

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
 
+const mongoose = require('mongoose'); 
+
+mongoose.connect('mongodb+srv://admin:YOoGqHQOt178jgUf@cluster0.bm69yrb.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 const app = express();
 
 app.use(express.json());
