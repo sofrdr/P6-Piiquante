96d66234e854eb7b8bf73541664ced4a44b698c7
Sophie Roudier <sophie.roudier@hotmail.fr>
2022-06-15 12:33:00 +0200

    Création middlewares createSauce, getSauces et getOneSauce

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+const sauceCtrl = require('../controllers/sauce');
 
-router.get('/', auth,);
-router.get('/:id', auth,);
-router.post('/', auth,);
+router.get('/', auth, sauceCtrl.getSauces);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
 router.put('/:id', auth,);
 router.delete('/:id', auth,);
 router.post('/:id/like', auth,);
